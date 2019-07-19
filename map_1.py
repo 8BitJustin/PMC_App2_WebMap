09e4cfd66f03623437f19ce37396c6556f752d53
8bitjustin <j.olson.digital@gmail.com>
2019-07-18 21:47:31 -0700

    Added marker and featuregroup code to .py file.

--- map_1.py
+++ map_1.py
@@ -1,5 +1,12 @@
 import folium
 
-map = folium.Map(location=[32.338879, -110.976696], zoom_start=10, tiles="Stamen Terrain")
+map = folium.Map(location=[32.338879, -110.976696], zoom_start=10,
+                 tiles="Stamen Terrain")
+
+fg = folium.FeatureGroup(name="My Map")
+fg.add_child(folium.Marker(location=[32.338879, -110.976696],
+                            popup="Hi, I'm a marker!",
+                            icon=folium.Icon(color="green")))
+map.add_child(fg)
 
 map.save("map_1.html")
