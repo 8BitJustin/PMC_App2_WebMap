2008e964f10bbfa0b0d17e6d31c1f2abf377a2b4
Justin Olson <j.olson.digital@gmail.com>
2019-07-21 16:58:58 -0700

    122 finished. Added name varible.

--- map_1.py
+++ map_1.py
@@ -4,15 +4,17 @@ import pandas
 data = pandas.read_csv("Volcanoes.txt")
 lat = list(data["LAT"])
 lon = list(data["LON"])
+name = list(data["NAME"])
+elev = list(data["ELEV"])
 
-map = folium.Map(location=[32.295760, -110.979765], zoom_start=11,
+map = folium.Map(location=[41.988748, -112.635234], zoom_start=5,
                  tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
-for lt, ln in zip(lat, lon):
+for lt, ln, nm, el in zip(lat, lon, name, elev):
     fg.add_child(folium.Marker(location=[lt,ln],
-                               popup="Hi, I'm a marker!",
+                               popup=nm + " " + str(el),
                                icon=folium.Icon(color="blue")))
 
 map.add_child(fg)
