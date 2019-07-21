e2794d3eb9c4a4a0699ee2c39d777384317f15e1
Justin Olson <j.olson.digital@gmail.com>
2019-07-21 10:40:17 -0700

    Finished 120, added basic coordinates via for loop. Added volcano file to folder.

--- map_1.py
+++ map_1.py
@@ -1,12 +1,15 @@
 import folium
 
-map = folium.Map(location=[32.338879, -110.976696], zoom_start=10,
+map = folium.Map(location=[32.295760, -110.979765], zoom_start=11,
                  tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
-fg.add_child(folium.Marker(location=[32.338879, -110.976696],
-                            popup="Hi, I'm a marker!",
-                            icon=folium.Icon(color="green")))
+
+for coordinates in [[32.338879, -110.976696], [32.226277, -111.001049]]:
+    fg.add_child(folium.Marker(location=coordinates,
+                               popup="Hi, I'm a marker!",
+                               icon=folium.Icon(color="blue")))
+
 map.add_child(fg)
 
 map.save("map_1.html")
