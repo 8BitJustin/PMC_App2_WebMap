32c0bc5a1d293c022c3d4119b8c1aba66b2274e3
Justin Olson <j.olson.digital@gmail.com>
2019-07-23 07:18:24 -0700

    Added GeoJson line of code to open world.json file.

--- map_1.py
+++ map_1.py
@@ -7,6 +7,7 @@ lon = list(data["LON"])
 name = list(data["NAME"])
 elev = list(data["ELEV"])
 
+
 def color_prod(elevation):
     if elevation < 1000:
         return 'green'
@@ -15,18 +16,20 @@ def color_prod(elevation):
     else:
         return 'red'
 
-map = folium.Map(location=[41.988748, -112.635234], zoom_start=5,
-                 tiles="Stamen Terrain")
+
+map = folium.Map(location=[41.988748, -112.635234], zoom_start=5, tiles="Stamen Terrain")
 
 fg = folium.FeatureGroup(name="My Map")
 
 for lt, ln, nm, el in zip(lat, lon, name, elev):
     fg.add_child(folium.CircleMarker(location=[lt,ln],
-                                    radius=6,
-                                    popup=nm + " " + str(el),
-                                    fill_color=color_prod(el),
-                                    color='grey',
-                                    fill_opacity=0.7))
+                                     radius=6,
+                                     popup=nm + " " + str(el),
+                                     fill_color=color_prod(el),
+                                     color='grey',
+                                     fill_opacity=0.7))
+
+fg.add_child(folium.GeoJson(data=(open("world.json", "r", encoding="utf-8-sig").read())))
 
 map.add_child(fg)
 
